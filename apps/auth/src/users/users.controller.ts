470f6ea726547e379d6b6bc0418a8d1fd68247be
Sonny <sonny@contrlswe.onmicrosoft.com>
2023-11-27 19:53:46 +0100

    mmm

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { CurrentUser, UserDocument } from '@app/common';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getUser(@CurrentUser() currentUser: UserDocument) {
+    return currentUser;
+  }
+}
---
+++ apps/payments/src/payments.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, UsePipes, ValidationPipe } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateChargeDto } from '@app/common';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
+
+@Controller()
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @MessagePattern('create_charge')
+  @UsePipes(new ValidationPipe())
+  async createCharge(@Payload() data: PaymentsCreateChargeDto) {
+    return this.paymentsService.createCharge(data);
+  }
+}
